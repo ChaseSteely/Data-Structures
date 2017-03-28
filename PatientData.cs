3f2d3be709914cc62af169194c616dcec1060883
Marshall Chase Steely <chasesteely@gmail.com>
2017-03-27 21:24:35 -0400

    Add files via upload

---
+++ PatientData.cs
@@ -0,0 +1,140 @@
+﻿using SimpleLinkedList;
+using System;
+using System.IO;
+
+namespace PatentData
+{
+    class PatientData
+    {
+        private SimpleLinkedList<Patient>[] list;
+        public int size { get; set; }
+        /// <summary>
+        /// Constructor.  Instantiates the array based on the size passed to the constructor.
+        /// </summary>
+        /// <param name="size">The size of the array.</param>
+        public PatientData(int size)
+        {
+            this.size = size;
+            list = new SimpleLinkedList<Patient>[size];
+        }
+
+        /// <summary>
+        /// Loads patients into the data structure from the filename passed as a parameter.
+        /// </summary>
+        /// <param name="filename">Path to the file where the patient data is kept.  The format is: <para>first,last,procedureCost</para></param>
+        public void LoadPatients(string filename)
+        {
+            FileStream fs = new FileStream(filename, FileMode.Open);
+            StreamReader input = new StreamReader(fs);
+
+            while (!input.EndOfStream)
+            {
+                string line = input.ReadLine();
+                string[] fields = line.Split(",".ToCharArray());
+                // TODO create the new patient
+                string last = fields[0];
+                string first = fields[1];
+                double cost = Double.Parse(fields[2]);
+                Patient p = new Patient(first, last, cost);
+                // TODO add the patient to the list
+                AddPatient(p);
+          
+            }
+        }
+
+        /// <summary>
+        /// Adds a patient to the data structure.
+        /// </summary>
+        /// <param name="patient">The patient to be added.</param>
+        public void AddPatient(Patient patient)
+        {
+
+            int index = Patient.genPatientId(patient.FirstName, patient.LastName) % this.size;
+            SimpleLinkedList<Patient> pList = list[index];
+            if (pList == null)
+            {
+                pList = new SimpleLinkedList<Patient>();
+                pList.AddAtHead(patient);
+                list[index] = pList;
+            }
+            else
+             {
+                pList.AddAtHead(patient);
+             }
+
+        }
+
+        public Patient FindPatientByName(string first, string last)
+        {
+            Patient pat = null;
+            
+            int index = Patient.genPatientId(first, last) % this.size;
+            SimpleLinkedList<Patient> pList = list[index];
+            if (pList != null)
+            {
+                pat = pList.Find(new Patient(first, last, 0));
+            }
+      
+            return pat;
+        }
+        
+        public int FindLongestLength()
+        {
+            int max = 0;
+           for(int i = 0; i < this.size; i++)
+            {
+                if (list[i] != null)
+                {
+                    int lSize = list[i].Count;
+                    if (lSize > max)
+                    {
+                        max = lSize;
+                    }
+                }
+            }
+            return max;
+        }
+
+        public double usedSlots()
+        {
+            double numSlots = 0.0;
+            for (int i = 0; i < this.size; i++)
+            {
+                if(list[i] != null)
+                {
+                    numSlots++;
+                }
+            }
+            double perc = (numSlots / this.size) * 100;
+            return perc;
+        }
+
+        public double avgSlots()
+        {
+            int numSlots = 0;
+            int sum = 0;
+            for (int i = 0; i < this.size; i++)
+            {
+                if (list[i] != null)
+                {
+                    numSlots++;
+                    sum += list[i].Count;
+                   
+                }
+            }
+            double aList = (sum / numSlots);
+            return aList;
+        }
+
+        public Patient RemovePatientById(string first, string last)
+        {
+            Patient pat = FindPatientByName(first, last);
+            int index = Patient.genPatientId(first, last) % this.size;
+            if (list[index]!= null)
+            {
+                list[index].Remove(pat);
+            }
+            return pat;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatentData
+{
+    class Program
+    {
+        private const int LIST_SIZE = 200;
+
+        static void showPatient(Patient p)
+        {
+      
+            Console.WriteLine("Name: {0}, ID: {1}, Cost: {2}", p.FirstName + " " + p.LastName, p.Id, p.ProcedureCost);
+        }
+
+        static void Main(string[] args)
+        {
+            PatientData data = new PatientData(LIST_SIZE);
+            data.LoadPatients("C:/Users/Marshall/Desktop/patientList.csv");
+            String action = "";
+         
+            while (!action.ToLower().StartsWith("q"))
+            {
+                Console.WriteLine("(F)ind, (S)tatistics, (R)emove (Q)uit");
+              
+                Console.WriteLine("Choose from the Menu above");
+                action = Console.ReadLine();
+                switch (action)
+                {
+                    case "f":
+                    case "F":
+                        {
+                            Console.WriteLine("Enter first and last name:");
+                            string name = Console.ReadLine();
+                           
+                                String[] names = name.Split();
+                                if (names.Length != 2)
+                                {
+                                    Console.WriteLine("Please enter a First and Last Name ONLY.");
+                                    continue;
+                                }
+
+                                Patient.Comparisons = 0;
+                                Patient pat = data.FindPatientByName(names[0], names[1]);
+                                if (pat != null)
+                                {
+                                    showPatient(pat);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Patient not found");
+                                }
+                                Console.WriteLine("Comparisions: {0}", Patient.Comparisons);
+                        }break;
+                    case "s":
+                    case "S":
+                        {
+                            Console.WriteLine("Number of Slots in the Array: {0} \n Percentage of Used Slots: {1}\n Max Length of LinkedList: {2}\n Avg Length of LinkedList: {3}\n", data.size, data.usedSlots(),
+                                data.FindLongestLength(), data.avgSlots());
+                        }
+                        break;
+
+                    case "q":
+                    case "Q":
+                        {
+
+                        }return;
+                }
+            }//END SWITCH
+
+            Console.ReadLine();
+        }
+    }
+}
